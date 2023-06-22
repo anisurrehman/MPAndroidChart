b38a8470718b3f014117f8a1fb55b9eb6a8c1777
anisurrehman <zunnoon11@gmail.com>
2023-06-22 15:33:14 +0530

    Update AxisRenderer.java

--- MPChartLib/src/main/java/com/github/mikephil/charting/renderer/AxisRenderer.java
+++ MPChartLib/src/main/java/com/github/mikephil/charting/renderer/AxisRenderer.java
@@ -187,7 +187,7 @@ public abstract class AxisRenderer extends Renderer {
 
         // force label count
         if (mAxis.isForceLabelsEnabled()) {
-            Log.e("AxisRenderer", "forece enabled");
+            Log.e("AxisRenderer", "forece enabled >>");
             interval = (float) range / (float) (labelCount - 1);
             if (mAxis.isGranularityEnabled())
                 interval = interval < mAxis.getGranularity() ? mAxis.getGranularity() : interval;
